359792d788160d38d44e45454c5671465cb8c25d
Shubham Sharma <ss9109@srmist.edu.in>
2025-05-20 00:25:55 +0530

    TEST_RULE-2

--- test_files/samplefile1.cpp
+++ test_files/samplefile1.cpp
@@ -1,17 +1,5 @@
 // test_files/sample.cpp
 #include <iostream>
-void longFunction() {
-  for (int i = 0; i < 35; ++i) {
-    std::cout << i << std::endl;
-  }
-}
-
-void longlongFunction(){
-      for (int i = 0; i < 45; ++i) {
-    std::cout << i << std::endl;
-  }
-}
-
 void longlonglongFunction(){
       for (int i = 0; i < 45; ++i) {
     std::cout << i << std::endl;
@@ -19,8 +7,6 @@ void longlonglongFunction(){
 }
 int main() {
     std::cout << "Hello from sample.cpp!" << std::endl;
-    longFunction();
-    longlongFunction();
     longlonglongFunction();
     return 0;
 }
